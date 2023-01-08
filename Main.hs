23454a61a896eec8b06c5fe6f4c70d4d6a56579c
ZhenbangYou <82886739+ZhenbangYou@users.noreply.github.com>
2023-01-07 18:24:09 -0800

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -9,12 +9,12 @@ import Control.Monad (replicateM)
 import Data.Array (Array)
 import Data.Array.IArray (IArray, amap, bounds, elems, listArray, (!))
 import Data.Ix (Ix)
+import Data.List (foldl1')
 import Data.List.Split (chunksOf)
 import Data.String.Interpolate (i)
 import Data.Time.Clock (diffUTCTime, getCurrentTime)
 import System.Directory (createDirectoryIfMissing)
 import System.Random (randomIO)
-import Data.List (foldl1')
 
 (|>) :: t1 -> (t1 -> t2) -> t2
 x |> f = f x
@@ -49,7 +49,7 @@ listToString :: Show a => [a] -> String
 listToString !xs =
   xs
     |> map show
-    |> foldl1' (++)
+    |> foldl1' (\a b -> a ++ " " ++ b)
 
 twoDimListToString :: Show a => [[a]] -> String
 twoDimListToString !xss =
@@ -78,10 +78,11 @@ outputOneCase (!inputHeight, !inputWidth, !weightHeight, !weightWidth) !path = d
   input <- randMatrix inputHeight inputWidth
   weight <- randMatrix weightHeight weightWidth
   let !output = convolution input weight
-      !outputDir = [i|#{inputHeight}x#{inputWidth}_#{weightHeight}x#{weightWidth}|]
-      !inputFile = path ++ outputDir ++ "input.txt"
-      !weightFile = path ++ outputDir ++ "weight.txt"
-      !outputFile = path ++ outputDir ++ "output.txt"
+      !outputDirPath = [i|#{path}/#{inputHeight}x#{inputWidth}_#{weightHeight}x#{weightWidth}|] :: String
+      !inputFile = [i|#{outputDirPath}/input.txt|]
+      !weightFile = [i|#{outputDirPath}/weight.txt|]
+      !outputFile = [i|#{outputDirPath}/output.txt|]
+  createDirectoryIfMissing True outputDirPath
   writeFile inputFile [i|#{inputHeight} #{inputWidth}\n|]
   writeFile weightFile [i|#{weightHeight} #{weightWidth}\n|]
   writeFile outputFile [i|#{inputHeight-weightHeight+1} #{inputWidth-weightWidth+1}\n|]
@@ -90,13 +91,11 @@ outputOneCase (!inputHeight, !inputWidth, !weightHeight, !weightWidth) !path = d
   appendFile outputFile $ twoDimListToString output
 
 outputCases :: [(Int, Int, Int, Int)] -> [Char] -> IO ()
-outputCases !shapeList !path = do
-  createDirectoryIfMissing True path
-  mapM_ (\x -> outputOneCase x path) shapeList
+outputCases !shapeList !path = mapM_ (\x -> outputOneCase x path) shapeList
 
 main :: IO ()
 main = do
   start <- getCurrentTime
-  outputCases [(256, 256, 16, 16)] "./cases"
+  outputCases [(128, 128, 16, 16),(128, 128, 32, 32)] "./cases"
   end <- getCurrentTime
   putStrLn [i|#{end `diffUTCTime` start} elapsed.|]
